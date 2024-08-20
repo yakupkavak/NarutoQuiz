67630c2a17a8293918d9503ec2175a3388d044ab
Yakup Kavak <yakupkavk@gmail.com>
2024-08-20 16:34:18 +0300

    ignore: ---

--- app/src/main/java/com/example/narutoquiz/ui/mainScreen/game/GameFragment.kt
+++ app/src/main/java/com/example/narutoquiz/ui/mainScreen/game/GameFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.narutoquiz.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
     private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
+    private val args: GameFragmentArgs by navArgs()
+    private val viewModel : GameViewModel by viewModels()
+    private var gameId: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        gameId = args.gameId
     }
 
     override fun onCreateView(
--- app/src/main/java/com/example/narutoquiz/ui/mainScreen/game/GameViewModel.kt
+++ app/src/main/java/com/example/narutoquiz/ui/mainScreen/game/GameViewModel.kt
@@ -1,4 +1,43 @@
 package com.example.narutoquiz.ui.mainScreen.game
 
-class GameViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.narutoquiz.ui.base.BaseViewModel
+
+class GameViewModel : BaseViewModel() {
+
+    private val _questionId = MutableLiveData<Int>()
+    val questionId : LiveData<Int> get() = _questionId
+
+    fun startGame(gameId: Int){
+        when(gameId) {
+            0 ->{
+
+            }
+            1->{
+
+            }
+            2->{
+
+            }
+            3->{
+
+            }
+        }
+    }
+    fun akatsukiGame(){
+
+    }
+    fun clanGame(){
+
+    }
+    fun familyGame(){
+
+    }
+    fun voiceActor(){
+        //4 farklı karakter aldım
+        //bunların bir tanesinin voice aktörünü aldım
+    }
+
+
 }
