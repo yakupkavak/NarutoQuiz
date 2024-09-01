81af6e9a458e8c3989e258db89a34bf28d8e7beb
Yakup Kavak <yakupkavk@gmail.com>
2024-09-01 14:25:07 +0300

    feat: add game over and challange mode

--- app/src/main/java/com/example/narutoquiz/ui/mainScreen/feedlist/UtilFunc.kt
+++ app/src/main/java/com/example/narutoquiz/ui/mainScreen/feedlist/UtilFunc.kt
@@ -6,8 +6,8 @@ import com.example.narutoquiz.data.model.FeedRowModel
 
 fun getRowModelList(): List<FeedRowModel> {
     return listOf(
-        FeedRowModel(description = "Classic", imageResId = R.drawable.akatsuki, gameId = 0),
-        FeedRowModel(description = "Characters", imageResId = R.drawable.akatsuki, gameId = 1),
+        FeedRowModel(description = "Challange", imageResId = R.drawable.akatsuki, gameId = 0),
+        FeedRowModel(description = "Classic", imageResId = R.drawable.akatsuki, gameId = 1),
         FeedRowModel(description = "Akatsuki", imageResId = R.drawable.akatsuki, gameId = 2),
         FeedRowModel(description = "Village", imageResId = R.drawable.akatsuki, gameId = 3),
         FeedRowModel(description = "Tailed Beast", imageResId = R.drawable.akatsuki, gameId = 4),
