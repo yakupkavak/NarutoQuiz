9daf9dbf6c4b75b14b3eeb46eaa92cf9d4c1c987
Yakup Kavak <yakupkavk@gmail.com>
2024-09-02 07:56:12 +0300

    refactor: add const

--- app/src/main/java/com/example/narutoquiz/ui/mainScreen/game/GameFragment.kt
+++ app/src/main/java/com/example/narutoquiz/ui/mainScreen/game/GameFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
